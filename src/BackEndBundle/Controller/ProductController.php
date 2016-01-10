f5128e217461f616d405b170674a224b78f920a5
patrxtar <petryshend@gmail.com>
2016-01-10 11:16:34 +0100

    Generate basic product entity

--- src/BackEndBundle/Controller/ProductController.php
+++ src/BackEndBundle/Controller/ProductController.php
@@ -6,8 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ProductController extends Controller
 {
-    public function indexAction()
+    public function listAction()
     {
-        return $this->render('BackEndBundle:Default:index.html.twig');
+        $products = $this->getDoctrine()
+            ->getRepository('BackEndBundle:Product')
+            ->findAll();
+
+        return $this->render(
+            'BackEndBundle:Default:index.html.twig',
+            [
+                'products' => $products,
+            ]
+        );
     }
 }
