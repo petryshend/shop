91359a8df6063019d8b294f3c9f5f989d08d1117
patrxtar <petryshend@gmail.com>
2016-01-10 19:09:18 +0100

    New product action

--- src/BackEndBundle/Controller/ProductController.php
+++ src/BackEndBundle/Controller/ProductController.php
@@ -2,10 +2,18 @@
 
 namespace BackEndBundle\Controller;
 
+use BackEndBundle\Entity\Product;
+use BackEndBundle\Form\ProductType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
+    /**
+     * @return Response
+     */
     public function listAction()
     {
         $products = $this->getDoctrine()
@@ -18,5 +26,36 @@ class ProductController extends Controller
                 'products' => $products,
             ]
         );
+    }
+
+    /**
+     * @param Request $request
+     * @return RedirectResponse|Response
+     */
+    public function newAction(Request $request)
+    {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $this->persistProduct($product);
+            return $this->redirectToRoute('back_end_product_list');
+        }
+        return $this->render(
+            '@BackEnd/product/new.html.twig',
+            [
+                'form' => $form->createView(),
+            ]
+        );
+    }
+
+    /**
+     * @param Product $product
+     */
+    private function persistProduct(Product $product)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($product);
+        $em->flush();
     }
 }
