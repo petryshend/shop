7190b8e6fdc99bc85df66570e92dbdd086777315
patrxtar <petryshend@gmail.com>
2016-01-17 19:45:08 +0200

    Generate User Entity

---
+++ src/BackEndBundle/Controller/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace BackEndBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserController extends Controller
+{
+    public function listAction()
+    {
+
+    }
+
+    public function newAction(Request $request)
+    {
+
+    }
+
+    public function editAction(Request $request, $id)
+    {
+
+    }
+
+    public function deleteAction($id)
+    {
+
+    }
+}
