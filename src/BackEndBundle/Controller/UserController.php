dc2530bec3c07cc7da5706aee9db2f5a06584a44
patrxtar <petryshend@gmail.com>
2016-01-23 11:04:41 +0200

    Implement basic security

--- src/BackEndBundle/Controller/UserController.php
+++ src/BackEndBundle/Controller/UserController.php
@@ -4,12 +4,13 @@ namespace BackEndBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
     public function listAction()
     {
-
+        return new Response('User List Action');
     }
 
     public function newAction(Request $request)
