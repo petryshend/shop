1d1caec424378e1b34d8f256c213f408f36fa582
dmitry <dpetrishen@experty.com>
2016-01-24 12:42:57 +0200

    Rempve sqlite db path from config; new and list users actions

--- src/BackEndBundle/Controller/UserController.php
+++ src/BackEndBundle/Controller/UserController.php
@@ -2,20 +2,51 @@
 
 namespace BackEndBundle\Controller;
 
+use BackEndBundle\Entity\User;
+use BackEndBundle\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
+    /**
+     * @return Response
+     */
     public function listAction()
     {
-        return new Response('User List Action');
+        $users = $this->getDoctrine()
+            ->getRepository('BackEndBundle:User')
+            ->findAll();
+
+        return $this->render(
+            '@BackEnd/user/list.html.twig',
+            [
+                'users' => $users,
+            ]
+        );
     }
 
+    /**
+     * @param Request $request
+     * @return RedirectResponse|Response
+     */
     public function newAction(Request $request)
     {
-
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->persistUser($user);
+            return $this->redirectToRoute('back_end_user_list');
+        }
+        return $this->render(
+            '@BackEnd/user/new.html.twig',
+            [
+                'form' => $form->createView(),
+            ]
+        );
     }
 
     public function editAction(Request $request, $id)
@@ -26,5 +57,15 @@ class UserController extends Controller
     public function deleteAction($id)
     {
 
+    }
+
+    /**
+     * @param User $user
+     */
+    private function persistUser(User $user)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
     }
 }
---
+++ src/BackEndBundle/Form/UserType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace BackEndBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('password')
+            ->add('save', SubmitType::class)
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'data_class' => 'BackEndBundle\Entity\User',
+            ]
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'user_type';
+    }
+}
