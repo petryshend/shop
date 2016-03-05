192c45048274f3896d6284c2a5e077a856a62434
petryshend <petryshend@gmail.com>
2016-03-05 12:53:09 +0200

    Beginning of product page

--- src/FrontEndBundle/Controller/FrontEndController.php
+++ src/FrontEndBundle/Controller/FrontEndController.php
@@ -3,6 +3,7 @@
 namespace FrontEndBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\DependencyInjection\Compiler\ResolveParameterPlaceHoldersPass;
 use Symfony\Component\HttpFoundation\Response;
 
 class FrontEndController extends Controller
@@ -20,6 +21,22 @@ class FrontEndController extends Controller
             'FrontEndBundle::index.html.twig',
             [
                 'products' => $products,
+            ]
+        );
+    }
+
+    /**
+     * @param int $id
+     * @return Response
+     */
+    public function productPageAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $product = $em->find('BackEndBundle:Product', $id);
+        return $this->render(
+            'FrontEndBundle::product.html.twig',
+            [
+                'product' => $product,
             ]
         );
     }
