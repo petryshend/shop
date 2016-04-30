96daf2782805ad890ea417c97106898c8f4b2be9
petryshend <petryshend@gmail.com>
2016-04-30 11:57:10 +0300

    Show cart modal on adding product to cart; show proper total price in modal

--- web/js/cart.js
+++ web/js/cart.js
@@ -1,5 +1,38 @@
 $(function() {
-    $('#add-to-cart-button').on('click', function() {
+    var calculateTotalPriceForCartItems = function(data) {
+        console.dir(data);
+        for (var i = 0; i < data.length; i++) {
+            var price = parseFloat(data[i].productPrice);
+            data[i].totalPrice = price * data[i].quantity;
+        }
+        return data;
+    };
+
+    var showCartModal = function() {
+        $.ajax({
+            url: '/cart/get',
+            type: 'POST'
+        }).done(function(data) {
+            if (data.length) {
+                data = JSON.parse(data);
+                data = calculateTotalPriceForCartItems(data);
+            } else {
+                data = '';
+            }
+            var template = $('#cart-table-template').html();
+            var rendered = Mustache.render(template,
+                {
+                    "items": data
+                }
+            );
+            $('#cart-table-placeholder').html(rendered);
+            $('#cartModal').modal('show');
+        }).fail(function() {
+            alert('fail');
+        });
+    };
+
+    var addItemToCart = function() {
         var productId = $('input[name=selected-product-id]').val();
         var productName = $('input[name=selected-product-name]').val();
         var productPrice = $('input[name=selected-product-price]').val();
@@ -13,31 +46,12 @@ $(function() {
                 quantity: 1
             }
         }).done(function(data) {
-            console.log(data);
+            showCartModal();
         }).fail(function() {
             alert('fail');
         });
-    }) ;
+    };
 
-    $('#show-cart-button').on('click', function() {
-        $.ajax({
-            url: '/cart/get',
-            type: 'POST'
-        }).done(function(data) {
-            if (data.length) {
-                data = JSON.parse(data);
-            } else {
-                data = '';
-            }
-            var template = $('#cart-table-template').html();
-            var rendered = Mustache.render(template,
-                {
-                    "items": data
-                }
-            );
-            $('#cart-table-placeholder').html(rendered);
-        }).fail(function() {
-            alert('fail');
-        });
-    });
+    $('#add-to-cart-button').on('click', addItemToCart) ;
+    $('#show-cart-button').on('click', showCartModal);
 });
